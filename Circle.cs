c89cdf050d03a40ce70f7f30b429d86b409fd0cf
KarinaChiste <163331375+KarinaChiste@users.noreply.github.com>
2025-02-27 14:59:10 -0700

    Circle Class Created

---
+++ Circle.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab5
+{
+    public class Circle
+    {
+        private double Radius { get; set; }
+        private const double MAX_RADIUS = 20;
+
+        public Circle(double newRadius)
+        {
+            if (newRadius < 0)
+            {
+                //throw exception
+            }
+            else if (newRadius > MAX_RADIUS)
+            {
+                // throw exception
+            }
+            else
+            {
+                Radius = newRadius;
+            }
+        }
+
+        public override string ToString()
+        {
+            return $"Circle[radius={Radius}]";
+        }
+    }
+}
